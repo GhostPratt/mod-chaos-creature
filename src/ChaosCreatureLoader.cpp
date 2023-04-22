24e2d239063c1ba01cbb1a1fcc28ad655e7e698f
phill <phillipprtt@gmail.com>
2023-04-22 15:21:42 -0400

    a

--- src/ChaosCreatureLoader.cpp
+++ src/ChaosCreatureLoader.cpp
@@ -8,7 +8,7 @@ void AddChaos_CreatureScripts();
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void Addmod_Chaos_CreatureScripts()
+void Addmod_chaos_creatureScripts()
 {
     AddChaos_CreatureScripts();
 }

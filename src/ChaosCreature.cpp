6c059163cd0c8bf12785e727be8b31d3f11ce6bd
phill <phillipprtt@gmail.com>
2023-05-12 16:51:34 -0400

    0

--- src/ChaosCreature.cpp
+++ src/ChaosCreature.cpp
@@ -113,7 +113,7 @@ public:
             //creature->AI()->EnterDefaultState();
         //}
         Creature* creature = new Creature();
-        creature->Create(item, 0, randx, randy, randz, 0.0f, map, 0, 0)
+        creature->Create(item, map, 0, item, 0, randx, randy, randz, 0.0f)
 
         creature->SetAIState(STATE_WANDER);
         creature->SetDefaultState(STATE_WANDER);

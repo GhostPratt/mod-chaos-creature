1198af4789711ea7b9419327fbfa9c1443dad777
phill <phillipprtt@gmail.com>
2023-05-12 11:51:44 -0400

    aa

--- src/ChaosCreature.cpp
+++ src/ChaosCreature.cpp
@@ -104,7 +104,7 @@ public:
 
         //sObjectMgr->AddCreatureToGrid(item, sObjectMgr->GetCreatureData(item))
 
-        Creature* creature = GetCreatureAI(item)->SummonCreature(randx, randy, randz, 0.0f, false, 0, 0);
+        Creature* creature = sSciprtMgr->GetCreatureAI(item)->SummonCreature(randx, randy, randz, 0.0f, false, 0, 0);
 
         if (creature)
         {

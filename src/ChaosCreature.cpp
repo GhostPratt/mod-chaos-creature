1634256f25511841dd3d81c10609294150fcf3c4
phill <phillipprtt@gmail.com>
2023-05-13 12:39:32 -0400

    dd

--- src/ChaosCreature.cpp
+++ src/ChaosCreature.cpp
@@ -108,6 +108,7 @@ public:
             //creature->AI()->EnterDefaultState();
         //}
         Creature* creature = new Creature();
+        uint32 guildLow = creature->GetGUIDLow();
         creature->Create(item, map, 0, item, 0, randx, randy, randz, 0.0f);
 
         creature->SetWanderDistance(100.0f);
